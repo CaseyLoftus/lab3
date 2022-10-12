b27710d756605e901a63a7d6971558d09128a49f
CaseyLoftus <114450184+CaseyLoftus@users.noreply.github.com>
2022-10-12 15:23:07 -0700

    Stirng

---
+++ ListTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public checkString implements StringChecker<String>{
+
+}
+public class LinkTests {
+    @Test
+    public void filterTest(){
+        List<String> result = new ArrayList<>();
+    }
+}
